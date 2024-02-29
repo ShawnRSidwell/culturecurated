481e4175591b1af99a79402cc61d93d73651fb79
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-02-28 17:10:21 -0800

    Changed rating area

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -3,7 +3,7 @@ import Card from "../ui/dashboard/card";
 export default function Home() {
   return (
     <main>
-      <div className="grid md:grid-cols-4 md:auto-cols-max gap-y-4 gap-x-">
+      <div className="mt-10 grid md:grid-cols-4 md:auto-cols-max gap-y-20 overflow-hidden">
         <Card />
         <Card />
         <Card />
--- app/ui/dashboard/card.tsx
+++ app/ui/dashboard/card.tsx
@@ -6,6 +6,7 @@ type Props = {};
 export default function Card({}: Props) {
   return (
     <div className=" w-[350px] h-[400px] flex flex-col items-center justify-start  p-3 ">
+      <p className="font-bold ">Best Places To Visit</p>
       <Image
         className="w-full h-1/2 rounded-lg"
         src={example}
@@ -13,29 +14,28 @@ export default function Card({}: Props) {
         height={500}
         alt=""
       />
-      <p className="font-bold ">Best Places To Visit</p>
-      <p className="text-sm ">
+      <div className="tracking-widest text-xs">🔵🔵🔵🔵🔵</div>
+      <p className="text-xs">225k Views • 2 months ago</p>
+      <p className="text-sm text-gray-500">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo
         dolor facere, dolore iure delectus, a atque nesciunt ipsa nihil amet
         sapiente exercitationem labore obcaecati officiis temporibus quod
         placeat? Maxime, quia.
       </p>
 
-      <div className="flex justify-between w-full">
-        <Image
-          className="bg-blue-100 w-8 h-8 rounded-full "
-          src={example}
-          width={500}
-          height={500}
-          alt=""
-        />
-        <div className="text-sm flex items-center justify-center">
-          Shawn Sidwell
+      <div className="flex justify-between w-full bg-slate-100 rounded p-2 mt-1">
+        <div className="text-sm flex items-center justify-center gap-1">
+          <Image
+            className="bg-blue-100 w-8 h-8 rounded-full "
+            src={example}
+            width={500}
+            height={500}
+            alt=""
+          />
+          <p className="ml-1 font-semibold">Shawn Sidwell</p>
         </div>
-        <div>
-          <div className="tracking-widest">🔵🔵🔵🔵🔵</div>
-          <div className="text-xs">225k Views • 2 months ago</div>
-          <div className="text-xs">Music</div>
+        <div className="text-sm flex items-center justify-center">
+          <p className="font-semibold">Music</p>
         </div>
       </div>
     </div>
