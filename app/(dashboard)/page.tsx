14465ea595961f6418c3e0ded8f76b482f2b77cb
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-02-28 17:57:38 -0800

    Create styling for category bar

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,13 @@
 import Card from "../ui/dashboard/card";
+import CategoryBar from "../ui/dashboard/categorybar";
 
 export default function Page() {
   return (
     <main>
-      <div className="mt-10 grid grid-cols-2 md:grid-cols-5 md:auto-cols-max gap-y-12 overflow-hidden">
+      <div className="p-3">
+        <CategoryBar />
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-5 md:auto-cols-max gap-y-12 overflow-hidden">
         <Card />
         <Card />
         <Card />
---
+++ app/ui/dashboard/categorybar.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+type Props = {};
+
+function CategoryBar({}: Props) {
+  return (
+    <div className="flex gap-3 overflow-clip">
+      <button className="bg-black text-white px-3 rounded h-8"> All</button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Travel
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Music
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Movies
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Games
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Background Music
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Travel
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Music
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Movies
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Games
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Background Music
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Travel
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Music
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Movies
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Games
+      </button>
+      <button className="bg-slate-200 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+        {" "}
+        Background Music
+      </button>
+    </div>
+  );
+}
+
+export default CategoryBar;
