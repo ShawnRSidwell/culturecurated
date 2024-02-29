6bb1d8e73a10820fe2a01e42a03119666809406a
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-02-29 12:14:03 -0800

    remove extra files

--- app/ui/dashboard/categorybar.tsx
+++ app/ui/dashboard/categorybar.tsx
@@ -6,111 +6,111 @@ function CategoryBar({}: Props) {
   return (
     <div className="flex gap-3 overflow-clip h-10 ml-3">
       <button className="bg-black text-white px-3 rounded"> All</button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Travel
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Music
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Movies
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Games
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Background Music
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Travel
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Music
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Movies
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Games
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Background Music
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Travel
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Music
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Movies
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Games
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Background Music
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Background Music
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Travel
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Music
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Movies
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Games
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Background Music
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Background Music
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Travel
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Music
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Movies
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Games
       </button>
-      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-400 hover:text-black">
+      <button className="bg-slate-100 px-3 text-black rounded hover:bg-slate-200 hover:text-black">
         {" "}
         Background Music
       </button>
