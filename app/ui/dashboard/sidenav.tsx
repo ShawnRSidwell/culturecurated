9f7c431c227898d3c1b6b33598960a0033175b3b
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-02-28 17:46:33 -0800

    Remove scrolling

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -8,7 +8,7 @@ import { MdOutlineStars } from "react-icons/md";
 
 export default function Sidenav() {
   return (
-    <div className="hidden h-screen flex-col  md:px-2 md:hover:overflow-auto md:block md:w-64 scroll-smooth">
+    <div className="hidden h-screen flex-col  md:px-2  md:block md:w-64 scroll-smooth">
       <div className="border-b-2 border-slate-400">
         <div className="flex flex-col items-center justify-center gap-2 py-2">
           <p className="h-10 w-full flex grow items-center justify-start leading-relaxed gap-3 pl-2">
