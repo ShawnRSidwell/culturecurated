227d86b90b6ce0e6e560417aeb1238416ec6a109
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-03-01 14:29:35 -0800

    Add styling to tiles in curation page

--- app/(dashboard)/curation/page.tsx
+++ app/(dashboard)/curation/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import testImage from "@/public/test-images/7f58450a663c6c99a07d97fe1c61344ej.webp";
+import Filter from "@/app/ui/curated/filter";
 
 export default function Page() {
   return (
-    <div className="p-5">
+    <div className="p-5 flex flex-col items-center justify-center">
       <div className="flex flex-col items-center justify-center ">
         <p className="text-3xl font-bold tracking-wide p-3 capitalize leading-2">
           Best business movies of 2024
@@ -22,94 +23,102 @@ export default function Page() {
           illum maiores?
         </p>{" "}
       </div>
-      <div className="grid grid-cols-3 justify-items-center p-5 gap-5">
-        <ol className="flex flex-col gap-3">
-          {[...Array(50)].map((_, index) => (
-            <li key={index} className="max-h-32 transition-all duration-300 ">
-              <p className="font-medium">
-                1. Wolf on WallStreet towers above all
-              </p>
-              <div className="grid grid-cols-4">
-                <div className="col-span-1 mt-2">
-                  <Image
-                    className="m-auto rounded-lg transition-all duration-300 z-1"
-                    src={testImage}
-                    alt=""
-                  />
+      <div className="mt-5">
+        <Filter />
+      </div>
+      <div className="p-5">
+        <p className="font-bold my-3 text-lg border-b-2">
+          Best Wall Street Movies
+        </p>
+        <div className="grid grid-cols-3 justify-items-center gap-5">
+          <ol className="flex flex-col gap-3">
+            {[...Array(50)].map((_, index) => (
+              <li key={index} className="max-h-32 transition-all duration-300 ">
+                <p className="font-medium">
+                  1. Wolf on WallStreet towers above all
+                </p>
+                <div className="grid grid-cols-4">
+                  <div className="col-span-1 mt-2">
+                    <Image
+                      className="m-auto rounded-lg transition-all duration-300 z-1"
+                      src={testImage}
+                      alt=""
+                    />
+                  </div>
+                  <div className="col-span-3 px-2 flex flex-col justify-around">
+                    <p className="text-xs">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Enim nisi suscipit beatae consequatur, aliquid autem
+                      veniam at nesciunt, eveniet distinctio voluptas in culpa
+                      consectetur similique et amet! Sint, illum maiores?
+                    </p>
+                    <a className="text-blue-500 underline text-xs " href="">
+                      www.testing.com
+                    </a>
+                  </div>
                 </div>
-                <div className="col-span-3 px-2 flex flex-col justify-around">
-                  <p className="text-xs">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Enim nisi suscipit beatae consequatur, aliquid autem veniam
-                    at nesciunt, eveniet distinctio voluptas in culpa
-                    consectetur similique et amet! Sint, illum maiores?
-                  </p>
-                  <a className="text-blue-500 underline text-xs " href="">
-                    www.testing.com
-                  </a>
+              </li>
+            ))}
+          </ol>
+          <ol className="flex flex-col gap-3">
+            {[...Array(50)].map((_, index) => (
+              <li key={index} className="  max-h-32">
+                <p className="font-medium">
+                  1. Wolf on WallStreet towers above all
+                </p>
+                <div className="grid grid-cols-4">
+                  <div className="col-span-1 mt-2">
+                    <Image
+                      className="m-auto rounded-lg hover:rounded-none transition-all duration-300 hover:scale-[500%] hover:translate-x-48 hover:border-2 hover:border-white"
+                      src={testImage}
+                      alt=""
+                    />
+                  </div>
+                  <div className="col-span-3 px-2 flex flex-col justify-around">
+                    <p className="text-xs">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Enim nisi suscipit beatae consequatur, aliquid autem
+                      veniam at nesciunt, eveniet distinctio voluptas in culpa
+                      consectetur similique et amet! Sint, illum maiores?
+                    </p>
+                    <a className="text-blue-500 underline text-xs " href="">
+                      www.testing.com
+                    </a>
+                  </div>
                 </div>
-              </div>
-            </li>
-          ))}
-        </ol>
-        <ol className="flex flex-col gap-3">
-          {[...Array(50)].map((_, index) => (
-            <li key={index} className="  max-h-32">
-              <p className="font-medium">
-                1. Wolf on WallStreet towers above all
-              </p>
-              <div className="grid grid-cols-4">
-                <div className="col-span-1 mt-2">
-                  <Image
-                    className="m-auto rounded-lg hover:rounded-none transition-all duration-300 hover:scale-[500%] hover:translate-x-48 hover:border-2 hover:border-white"
-                    src={testImage}
-                    alt=""
-                  />
+              </li>
+            ))}
+          </ol>
+          <ol className="flex flex-col gap-3">
+            {[...Array(50)].map((_, index) => (
+              <li key={index} className="  max-h-32">
+                <p className="font-medium">
+                  1. Wolf on WallStreet towers above all
+                </p>
+                <div className="grid grid-cols-4">
+                  <div className="col-span-1 mt-2">
+                    <Image
+                      className="m-auto rounded-lg hover:rounded-none transition-all duration-300 hover:scale-[500%]  hover:border-2 hover:border-white"
+                      src={testImage}
+                      alt=""
+                    />
+                  </div>
+                  <div className="col-span-3 px-2 flex flex-col justify-around">
+                    <p className="text-xs">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Enim nisi suscipit beatae consequatur, aliquid autem
+                      veniam at nesciunt, eveniet distinctio voluptas in culpa
+                      consectetur similique et amet! Sint, illum maiores?
+                    </p>
+                    <a className="text-blue-500 underline text-xs " href="">
+                      www.testing.com
+                    </a>
+                  </div>
                 </div>
-                <div className="col-span-3 px-2 flex flex-col justify-around">
-                  <p className="text-xs">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Enim nisi suscipit beatae consequatur, aliquid autem veniam
-                    at nesciunt, eveniet distinctio voluptas in culpa
-                    consectetur similique et amet! Sint, illum maiores?
-                  </p>
-                  <a className="text-blue-500 underline text-xs " href="">
-                    www.testing.com
-                  </a>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ol>
-        <ol className="flex flex-col gap-3">
-          {[...Array(50)].map((_, index) => (
-            <li key={index} className="  max-h-32">
-              <p className="font-medium">
-                1. Wolf on WallStreet towers above all
-              </p>
-              <div className="grid grid-cols-4">
-                <div className="col-span-1 mt-2">
-                  <Image
-                    className="m-auto rounded-lg hover:rounded-none transition-all duration-300 hover:scale-[500%]  hover:border-2 hover:border-white"
-                    src={testImage}
-                    alt=""
-                  />
-                </div>
-                <div className="col-span-3 px-2 flex flex-col justify-around">
-                  <p className="text-xs">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Enim nisi suscipit beatae consequatur, aliquid autem veniam
-                    at nesciunt, eveniet distinctio voluptas in culpa
-                    consectetur similique et amet! Sint, illum maiores?
-                  </p>
-                  <a className="text-blue-500 underline text-xs " href="">
-                    www.testing.com
-                  </a>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ol>
+              </li>
+            ))}
+          </ol>
+        </div>
       </div>
     </div>
   );
---
+++ app/ui/curated/filter.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+type Props = {};
+
+export default function Filter({}: Props) {
+  return (
+    <div className=" max-w-3xl w-full flex gap-4 items-center justify-center \">
+      <div>
+        <p>
+          <span className="font-medium">Category:</span> Movies
+        </p>
+      </div>
+      <div>
+        <label htmlFor="pet-select" className="p-2 font-medium">
+          Subcategories:
+        </label>
+        <select id="pet-select" className="bg-gray-50 rounded">
+          <option value="All">All</option>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+        </select>
+      </div>
+      <div>
+        <fieldset className="flex gap-2">
+          <label htmlFor="views" className="font-medium">
+            Views:
+          </label>
+          <label htmlFor="All">
+            <input type="radio" id="All" name="viewType" />
+            All
+          </label>
+          <label htmlFor="onlylistitems">
+            <input type="radio" id="list" name="viewType" />
+            List
+          </label>
+          <label htmlFor="onlytext">
+            <input type="radio" id="onlytext" name="viewType" />
+            Text
+          </label>
+        </fieldset>
+      </div>
+      <p>
+        <span className="font-medium">Rating:</span> 🖊🖊🖊🖊🖊
+      </p>
+    </div>
+  );
+}
